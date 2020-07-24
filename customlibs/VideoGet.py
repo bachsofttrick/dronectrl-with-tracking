dff49f98e8fd454d97f2baddf6d8a1a62461cfea
Bach Leu <xuanbach1307@yahoo.com.vn>
2020-07-24 09:44:57 +0700

    edit VideoGet to support udp, tcp from gstreamer

--- customlibs/VideoGet.py
+++ customlibs/VideoGet.py
@@ -19,6 +19,12 @@ class VideoGet:
         elif (src[0:4] == "rtsp"):
             src = "rtspsrc location=" + src + " latency=0 buffer-mode=auto ! decodebin ! videoconvert ! appsink sync=false"
             self._stream = cv2.VideoCapture(src)
+        elif (src[0:3] == "udp"):
+            src = "udpsrc port=" + src[4:] + " ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! appsink sync=false"
+            self._stream = cv2.VideoCapture(src)
+        elif (src[0:3] == "tcp"):
+            src = "tcpclientsrc " + src + " ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! appsink sync=false" #host=192.168.4.101 port=8081
+            self._stream = cv2.VideoCapture(src)
         (self.grabbed, self.frame) = self._stream.read()
         self._stopped = False
 
