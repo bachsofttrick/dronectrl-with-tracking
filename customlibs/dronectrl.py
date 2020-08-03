19e7d3b5332abf7e8eb0bb611ac1091a648f9b13
Bach Phan <40909481+bachsofttrick@users.noreply.github.com>
2020-08-03 10:29:43 +0700

    add control for naza drone

--- customlibs/dronectrl.py
+++ customlibs/dronectrl.py
@@ -170,3 +170,126 @@ class dm107s():
             self.onoff = 0
             self._calibrate_flag = True
             self._calibrate_timer = time()
+
+class naza():
+    # Default control value
+    def __init__(self, ip, port):
+        # 4 values for flight
+        self.roll = 8
+        self.pitch = 8
+        self.throttle = 8
+        self.yaw = 8
+        # Prevent multiple ignite button presses
+        self._ignite_flag = False
+        # Connect to UDP port
+        self.sess = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        #self.sess.connect((ip, port))
+        # Initialize timer value
+        self._ignite_timer = 0
+        # Flag to stop thread
+        self._stopped = False
+    
+    # Start separated thread for drone control
+    def start(self):    
+        self._thread = Thread(target=self.send_ctrl, args=(), daemon=True)
+        self._thread.start()
+        return self
+    
+    # Get command hex for drone
+    def get_hex(self):
+        # XOR is for checksum
+        self.command_out=(self.throttle<<12|self.yaw<<8|self.pitch<<4|self.roll)
+        self.command_out = hex(self.command_out)[2::]
+        return self.command_out
+    
+    # Send control to drone
+    def send_ctrl(self):
+        while not self._stopped:
+            self._package = self.get_hex()
+            #self.sess.send(self._package)
+            self.sess.sendto(self._package, (ip, port))
+            self.Flag_off()
+            sleep(0.02)
+    
+    # Close connection to drone
+    def close_connection(self):
+        self._stopped = True
+        if self._thread.daemon == False:
+            self._thread.join()
+            self.sess.close()
+    
+    # Return to default
+    def default(self):
+        self.roll = 8
+        self.pitch = 8
+        self.throttle = 8
+        self.yaw = 8
+        self._takeoff_flag = False
+    
+    # Increment control
+    def incremt(self, rl, pt, th, yw):
+        self._value_to_change = [8, 8, 8, 8]
+        self._change_val = [rl, pt, th, yw]
+        for x in range(len(self._value_to_change)):
+            self._value_to_change[x] += self._change_val[x]
+            if self._value_to_change[x] <= 0:
+                self._value_to_change[x] = 0
+            if self._value_to_change[x] >= 15:
+                self._value_to_change[x] = 15
+            [self.roll, self.pitch, self.throttle, self.yaw] = self._value_to_change
+    
+    # Roll right
+    def roll_right(self):
+        if self.roll < 14:
+            self.roll += 1
+    
+    # Pitch forward
+    def pitch_fwd(self):
+        if self.pitch < 15:
+            self.pitch += 1
+    
+    # Increase throttle
+    def throttle_up(self):
+        if self.throttle < 15:
+            self.throttle += 1
+    
+    # Yaw right
+    def yaw_right(self):
+        if self.yaw < 15:
+            self.yaw += 1
+    
+    # Roll left
+    def roll_left(self):
+        if self.roll > 0:
+            self.roll -= 1
+    
+    # Pitch backward
+    def pitch_bwd(self):
+        if self.pitch > 0:
+            self.pitch -= 1
+    
+    # Decrease throttle
+    def throttle_dwn(self):
+        if self.throttle > 0:
+            self.throttle -= 1
+    
+    # Yaw left
+    def yaw_left(self):
+        if self.yaw > 0:
+            self.yaw -= 1
+    
+    # Start engine
+    def ignite(self):
+        self.roll = 0
+        self.pitch = 0
+        self.throttle = 0
+        self.yaw = 15
+        self._takeoff_flag = True
+    
+    # Flip takeoff flag
+    def Flag_off(self):
+        if (self._takeoff_flag == True and (time() - self._takeoff_timer >= 1)):
+            self.default()
+
+
+    
